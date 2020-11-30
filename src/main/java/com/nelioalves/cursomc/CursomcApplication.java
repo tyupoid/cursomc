2c0ab96305189ac776dff97f59bfb536b61b9bcf
Thiago Castello <thiago_tyupoid@hotmail.com>
2020-11-30 15:48:52 -0300

    associacao muitos para muitos

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -8,12 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Produto;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
+import com.nelioalves.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -24,7 +29,20 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Categoria categoria1 = new Categoria(null, "informatica");
 		Categoria categoria2 = new Categoria(null, "escritorio");
-		categoriaRepository.saveAll(Arrays.asList(categoria1,categoria2));
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+
+		Produto produto1 = new Produto(null, "computador", 2000.0);
+		Produto produto2 = new Produto(null, "impressora", 800.0);
+		Produto produto3 = new Produto(null, "mouse", 80.0);
+		
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoria2.getProdutos().addAll(Arrays.asList(produto2));
+		
+		produto1.getCategorias().add(categoria1);
+		produto2.getCategorias().addAll(Arrays.asList(categoria1,categoria2));
+		produto3.getCategorias().add(categoria1);
+		
+		produtoRepository.saveAll(Arrays.asList(produto1,produto2,produto3));
 	}
 
 }
--- src/main/java/com/nelioalves/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package com.nelioalves.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository  extends JpaRepository<Categoria, Integer>{
-	
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
 }
